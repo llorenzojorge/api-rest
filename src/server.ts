a851a2888f60a1c7507182a58e5e87c14cd835b8
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-01-29 13:20:31 -0300

    Route Params

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,10 @@ const PORT = 3333
 
 const app = express()
 
-app.get("/", (request, response) => {
-  response.send("Hello World Express!")
-})
+app.get("/products/:id/:user", (request, response) => {
+  const { id, user } = request.params
 
-app.get("/products", (request, response) => {
-  response.send("produtos teste")
+  response.send(`Usuário: ${user} e Produto ${id}`)
 })
-
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
