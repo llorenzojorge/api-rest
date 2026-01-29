8a9c40a5a71b8b70bf94ba44d7992494c3fae4d5
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-01-29 09:49:05 -0300

    Enviando requisição com o método GET

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,13 @@ const PORT = 3333
 
 const app = express()
 
+app.get("/", (request, response) => {
+  response.send("Hello World Express!")
+})
+
+app.get("/products", (request, response) => {
+  response.send("produtos teste")
+})
+
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
