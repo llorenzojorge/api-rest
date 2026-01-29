3d9def4b73b9649db5cf37f6014d2cdc30284a19
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-01-29 14:48:04 -0300

    Dados no corpo da requisição

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,20 @@ import express from "express"
 const PORT = 3333
 
 const app = express()
+// Faz o Express esperar que os dados da requisição sejam do tipo JSON.
+app.use(express.json())
 
+// Método GET.
 app.get("/products", (request, response) => {
   const { page, limit } = request.query
   
   response.send(`Página ${page} de ${limit}`)
+})
+
+// Método POST.
+app.post("/products", (request, response) => {
+  const { name, price } = request.body
+  response.send(`o produto: ${name} custa R$${price}`)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
