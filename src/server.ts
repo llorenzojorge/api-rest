e2a35cda848b800488a27fa8e94509b8435ae3c8
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-01-30 14:00:20 -0300

    Modificando requisição e arquivo d.ts

--- src/server.ts
+++ src/server.ts
@@ -11,17 +11,17 @@ app.use(express.json())
 // app.use(myMiddleware)
 
 // Middleware local em uma rota especifica 
-app.get("/products", myMiddleware, (request, response) => {
+app.get("/products", (request, response) => {
   const { page, limit } = request.query
   
   response.send(`Página ${page} de ${limit}`)
 })
 
-app.post("/products", (request, response) => {
+app.post("/products", myMiddleware, (request, response) => {
   const { name, price } = request.body
 
   // response.send(`o produto: ${name} custa R$${price}`)
-  response.status(201).json({ name, price })
+  response.status(201).json({ name, price, user_id: request.user_id })
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
