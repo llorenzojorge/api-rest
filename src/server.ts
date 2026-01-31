f44a20f0c4aa902d742e024efcf6029d66c64761
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2026-01-31 19:54:10 -0300

    AppError (Erro Cliente x Erro Server)

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from "express"
+
 import { routes } from "./routes/index.js"
+import { AppError } from "./utils/AppError.js"
 
 const PORT = 3333
 
@@ -8,7 +10,15 @@ app.use(express.json()) // Faz o Express esperar que os dados da requisição se
 
 app.use(routes)
 
+
+/**
+ * 400(Bad Request): Erro do cliente.
+ * 500(Internal Server Erro): Erro interno do servidor. 
+ */
 app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message})
+  }
   response.status(500).json({ message: error.message })
 })
 
