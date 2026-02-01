f120aec6f0ea732ba7e29020a9a23af73593942d
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2026-02-01 12:28:16 -0300

    Customizar mensagem

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -19,8 +19,8 @@ class ProductsController {
 
   create(request: Request, response: Response) {
     const bodySchema = z.object({
-      name: z.string(),
-      price: z.number().nullish(),
+      name: z.string({ required_error: "Name is required!" }),
+      price: z.number({ required_error: "Price is required!" }),
     })
 
     const { name, price } = bodySchema.parse(request.body)
