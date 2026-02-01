1ff184d005e89f6890f860273904fe046e181ca5
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2026-02-01 13:33:42 -0300

    Adicionando regras de validação

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -19,8 +19,14 @@ class ProductsController {
 
   create(request: Request, response: Response) {
     const bodySchema = z.object({
-      name: z.string({ required_error: "Name is required!" }),
-      price: z.number({ required_error: "Price is required!" }),
+      name: z
+        .string({ required_error: "Name is required." })
+        .trim()
+        .min(6, { message: "Name must be 6 or more characters." }),
+      price: z
+        .number({ required_error: "Price is required." })
+        .positive({ message: "Price must be positive." })
+        .gte(1), // Definir valor maior ou igual ao paramêtro informado.
     })
 
     const { name, price } = bodySchema.parse(request.body)
